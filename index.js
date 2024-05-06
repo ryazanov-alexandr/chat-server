6bc66aace230484d629a468f39735122448062c3
ryazanov-alexandr <ryazanov.a1exandr@yandex.ru>
2024-05-06 11:41:24 +0300

    init server

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+dotenv.config(); 
+
+const io = require("socket.io")(server, {
+    cors: {
+      origin: process.env.CORS_ORIGIN.split(','),
+      methods: ["GET", "POST"]
+    },
+    maxHttpBufferSize: 1e7
+  });
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log("start listenning "+ PORT);
+})
+
+let connectedUsers = []; 
+
+io.on('connection', onConnected);
+
+function onConnected(socket) {
+    socket.on('connect user', (data) => {
+        data.socketId = socket.id;
+        connectedUsers.push(data);
+        io.emit('connect user', connectedUsers);
+    })
+
+    socket.on('chat message', (data) => {
+        socket.broadcast.emit('chat message', data);
+    })
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data);
+    })
+
+    socket.on('disconnect', () => {
+        connectedUsers = connectedUsers.filter(user => user.socketId != socket.id);
+        io.emit('connect user', connectedUsers);
+    })
+}
